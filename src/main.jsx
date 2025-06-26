eb9ef5a1fa0f8af88152495c401112bf44b88529
Victoriahjs <tabvictoriacristina@gmail.com>
2025-06-25 23:43:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'src/app.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<app.jsx/>);
